48ab71e788e51045fbce4fa62f83fca926169990
Neel Smith <dnsmith.neel@gmail.com>
2021-11-19 07:23:40 -0500

    Set up file skeleton

--- src/yaml.jl
+++ src/yaml.jl
@@ -1,4 +1,10 @@
 
+function header(src)
+    parts = split(src, r"^---")
+    contentparts = filter(s -> ! isempty(s), parts)
+    length(contentparts) > 1 ? contentparts[1] : nothing
+end
+
 function settings(src)
     nothing
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,18 @@ using CiteDown
 using CommonMark
 using Test
 
-@testset "Test finding YAML settings" begin
+@testset "Test extracting YAML header from string" begin
+
+    src = """
+    ---
+    ict: "http://www.homermultitext.org/ict2/?"
+    iiif: 
+        service: "http://www.homermultitext.org/iipsrv"
+        path: "/project/homer/pyramidal/deepzoom
+    ---
+    
+    Text body follows
+"""
+    yamlsrc = 
     @test_broken "Results" == "expected"
 end
