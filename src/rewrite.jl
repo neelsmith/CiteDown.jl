be02eac51c1c807dbab00334f9431f24adb5b73a
Neel Smith <dnsmith.neel@gmail.com>
2021-11-23 09:05:26 -0500

    Fix bug

--- src/rewrite.jl
+++ src/rewrite.jl
@@ -6,59 +6,82 @@ $(SIGNATURES)
 """
 function rewrite(f)
     (yaml, md) = pageparts(f)
-    cdsettings = settings(yaml)
+    cdsettings = CiteDown.settings(yaml)
     urlize(md, cdsettings)
 end
 
-
+"""Instantiate a `IIIFservice` based on settings.
+$(SIGNATURES)
+"""
 function iiifservice(cdsettings::CiteDown.Settings)
     IIIFservice(cdsettings.iiifurl, cdsettings.iiifpath)
 end
-"""Compose markdown wrapping IIIF URL for image linked to an image citation tool installation.
 
-$(SIGNATURES)
 
-# Arguments
 
-- `img` URN string
-- `cdsettings` CiteDown.Settings
+function rewrite_img(n,cdsettings::CiteDown.Settings)
+    iiif = iiifservice(cdsettings)
+    u = Cite2Urn(n.t.destination)
+    imgdest = url(u, iiif, ht = cdsettings.maxheight)
 
-"""
-function linkedMarkdownImage(img::AbstractString, cdsettings::Settings)
-    srvc = iiifservice(cdsettings) #IIIFservice(cdsettings.iiifurl, cdsettings.iiifpath)
-    linkedMarkdownImage(cdsettings.ict, Cite2Urn(img), srvc; ht = cdsettings.maxheight, caption = "image")
+    newimg =  CommonMark.Node(CommonMark.Image())
+    newimg.t.destination = imgdest
+    caption = n.first_child.literal
+    captionnode = CommonMark.text("Rewrittten link to " * caption)
+    CommonMark.append_child(newimg, captionnode)
+
+
+    ictnode = CommonMark.Node(CommonMark.Link())
+    ictdest = cdsettings.ict * "urn=" * n.t.destination
+    ictnode.t.destination = ictdest
+
+    CommonMark.append_child(ictnode, newimg)
+
+    CommonMark.insert_before(n, ictnode)
+  
 end
 
 """Convert URNs in markdown image references to URL values.
 $(SIGNATURES)
 """
-function urlize(md, cdsettings::Settings)
+function urlize(md::AbstractString, cdsettings::Settings)
     p = Parser()
     parsed = p(md)
+    urlize(parsed, cdsettings)
+    
+end
+
+"""Convert URNs in markdown image references to URL values.
+$(SIGNATURES)
+"""
+function urlize(parsed::CommonMark.Node, cdsettings::Settings)
+    rmlist = []
     for (node, enter) in parsed
         if enter
+            @info(node.t)
             if node.t isa CommonMark.Image
-                linked = linkedMarkdownImage(node.t.destination, cdsettings)
-                u = Cite2Urn(node.t.destination)
-                
-                
-                newdest = url(u, iiifservice(cdsettings), ht = cdsettings.maxheight)
-                
-                
-                ictlink = cdsettings.ict * "urn=" * node.t.destination
-                @info("New dest ", newdest)
-                @info("ICT ", ictlink)
-                
-                #node.t.destination = newdest
-                #@info("Rewrite image ", node.t.destination, linked) 
+                @info("Rewrite ", node.t.destination)
+                CiteDown.rewrite_img(node, cdsettings)
+                @info("Delete ", node.t)
+                push!(rmlist, node)
             end
         end
     end
+    for n in rmlist
+        CommonMark.unlink(n)
+    end
     # Format rewritten AST as a markdown string:
+    ast2markdown(parsed)
+end
+
+"""Format AST as a markdown string.
+
+$(SIGNATURES)
+"""
+function ast2markdown(parsed)
     iobuf = IOBuffer()
     env = Dict{String,Any}()
     writer = CommonMark.Writer(CommonMark.Markdown(iobuf), iobuf, env)
     CommonMark.write_markdown(writer, parsed)
     String(take!(iobuf))
 end
-
